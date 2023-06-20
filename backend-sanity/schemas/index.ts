67d5ce795f9813ccd0b6f573ae2ee2362ee66bdd
DusanVelimirovic <dusan.velimirovic@yahoo.com>
2023-06-20 15:27:42 +0200

    Finished backend schemas

--- backend-sanity/schemas/index.ts
+++ backend-sanity/schemas/index.ts
@@ -1,7 +1,15 @@
 
-import testimonials from './testimonials'
+import testimonials from './testimonials';
+import aboutus from './aboutus';
+import brands from './brands';
+import contact from './contact';
+import experience from './experiences';
+import skills from './skills';
+import workExperience from './workExperience';
+import works from './works';
 
 
 
 // Then we give our schema to the builder and provide the result to Sanity
-export const schemaTypes = [testimonials]
+export const schemaTypes = [testimonials, aboutus, brands, contact,
+experience, skills, workExperience, works]
