6c7c26beda106896446038df27516dd7d1840529
Dusan Velimirovic <dusan.velimirovic@yahoo.com>
2023-06-22 08:38:11 +0200

    js

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Navbar.scss";
 import { images } from "../../constants";
 
+// Import libraries for Hamburger Menu
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
+
 const Navbar = () => {
+  // State block - keep track of all changes on the page
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav className='app__navbar'>
         <div className='app__navbar-logo'>
@@ -17,6 +24,28 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+        {/* Hamburger Menu for mobile devices */ }
+        <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: 'easeOut' }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                <li key={item}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
     </nav>
 
   )
